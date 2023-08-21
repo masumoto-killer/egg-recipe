29ac9b08a97d2fa69c321a8c36f8f60490e6e49c
chinq-nal <123537580+masumoto-killer@users.noreply.github.com>
2023-08-21 18:05:38 +0900

    update entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,9 +7,13 @@ composer install
 php artisan key:generate
 php artisan migrate
 php artisan db:seed
-
 php artisan optimize:clear
 php artisan optimize
+php artisan config:clear
+php artisan cache:clear
+composer dump-autoload
+php artisan view:clear
+php artisan route:clear
 
 service nginx start
 
