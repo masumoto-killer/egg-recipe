c18f12b13e48185e79c3244a130e574aebc19481
chinq-nal <123537580+masumoto-killer@users.noreply.github.com>
2023-08-22 12:54:36 +0900

    update git ignore

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,8 +1,8 @@
 @extends('app')
 
 @section('content')
-    <div class="container d-flex align-items-center justify-content-center vh-100">
-        <div class="col-md-5">
+    <div class="container d-flex align-items-center justify-content-center mt-4">
+        <div class="col-lg-5 col-md-10 col-sm-">
             <div class="card">
                 <div class="card-header">{{ __('Đăng ký') }}</div>
                 <div class="card-body">
@@ -27,8 +27,8 @@
 
                         <div class="form-group row mb-4 justify-content-between">
                             <label for="last_period_end" class="col-auto col-form-label">
-                                {{ __('Ngày cuối cùng vỡ trứng') }}</label>
-                            <div class="col-lg-5 col-md-12 col-sm-12">
+                                {{ __('Ngày cuối cùng vỡ trứng:') }}</label>
+                            <div class="col-lg-4 col-md-12 col-sm-12">
                                 <input id="last_period_end" type="date" class="form-control" name="last_period_end" required>
                             </div>
                         </div>
@@ -36,7 +36,7 @@
                         <div class="form-group row mb-4 justify-content-between">
                             <label for="average_cycle_length" class="col-auto col-form-label">
                                 {{ __('Chu kỳ rớt trứng bình thường:') }}</label>
-                            <div class="col-lg-5 col-md-12 col-sm-12">
+                            <div class="col-lg-4 col-md-12 col-sm-12">
                                 <div class="input-group">
                                     <input id="average_cycle_length" type="number" class="form-control" name="average_cycle_length" value="28" required>
                                     <div class="input-group-text">ngày</div>
@@ -47,7 +47,7 @@
                         <div class="form-group row mb-4 justify-content-between">
                             <label for="average_period_length" class="col-auto col-form-label">
                                 {{ __('Số ngày vỡ trứng bình thường:') }}</label>
-                            <div class="col-lg-5 col-md-12 col-sm-12">
+                            <div class="col-lg-4 col-md-12 col-sm-12">
                                 <div class="input-group">
                                     <input id="average_period_length" type="number" class="form-control" name="average_period_length" value="5" required>
                                     <div class="input-group-text">ngày</div>
